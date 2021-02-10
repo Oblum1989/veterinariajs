9c186e6c09782e8982dbffea552d469dbc23a35c
oscar blum <oscar.blum@gmail.com>
2021-02-09 23:44:30 -0500

    listtar mascotas

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,1 +1,36 @@
-console.log("mascota")
+const listPets = document.getElementById("list-pets")
+let pets = [
+  {
+    tipo: "Gato",
+    name: "Manchas",
+    owner: "Juan",
+  },
+  {
+    tipo: "Perro",
+    name: "firulais",
+    owner: "Jose",
+  },
+  {
+    tipo: "Perro",
+    name: "Furia",
+    owner: "Sandra",
+  },
+];
+
+function showPets() {
+  const htmlPets = pets.map(
+    (pet, key) => `
+    <tr>
+      <th scope="row">${key}</th>
+      <td>${pet.tipo}</td>
+      <td>${pet.name}</td>
+      <td>${pet.owner}</td>
+      <td>
+        <button type="button" class="btn btn-success"><i class="far fa-edit"></i></button>
+        <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+      </td>
+    </tr>`).join("")
+    listPets.innerHTML = htmlPets
+}
+
+showPets()
