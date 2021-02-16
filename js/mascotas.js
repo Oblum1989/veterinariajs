80814dade2feb4e0321ea12ccb25eb62c0827bba
oscar blum <oscar.blum@gmail.com>
2021-02-15 21:25:36 -0500

    add delete pet

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,10 +1,10 @@
-const listPets = document.getElementById("list-pets")
-const type = document.getElementById("tipo")
-const name = document.getElementById("nombre")
-const owner = document.getElementById("dueno")
-const inputId = document.getElementById("inputId")
-const form = document.getElementById("form")
-const btnSave = document.getElementById("btnSave")
+const listPets = document.getElementById("list-pets");
+const type = document.getElementById("tipo");
+const name = document.getElementById("nombre");
+const owner = document.getElementById("dueno");
+const inputId = document.getElementById("inputId");
+const form = document.getElementById("form");
+const btnSave = document.getElementById("btnSave");
 let pets = [
   {
     type: "Gato",
@@ -24,8 +24,9 @@ let pets = [
 ];
 
 function showPets() {
-  const htmlPets = pets.map(
-    (pet, key) => `
+  const htmlPets = pets
+    .map(
+      (pet, key) => `
     <tr>
       <th scope="row">${key}</th>
       <td>${pet.type}</td>
@@ -33,52 +34,65 @@ function showPets() {
       <td>${pet.owner}</td>
       <td>
         <button type="button" class="btn btn-success edit" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="far fa-edit"></i></button>
-        <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-danger delete"><i class="far fa-trash-alt"></i></button>
       </td>
-    </tr>`).join("")
-  listPets.innerHTML = htmlPets
-  Array.from(document.getElementsByClassName("edit")).forEach((btnEdit, key)=>btnEdit.onclick = edit(key))
+    </tr>`
+    )
+    .join("");
+  listPets.innerHTML = htmlPets;
+  Array.from(document.getElementsByClassName("edit")).forEach(
+    (btnEdit, key) => (btnEdit.onclick = edit(key))
+  );
+  Array.from(document.getElementsByClassName("delete")).forEach(
+    (btnDelete, key) => (btnDelete.onclick = deletePet(key))
+  );
 }
 
 function sendData(e) {
-  e.preventDefault()
+  e.preventDefault();
   const data = {
     type: type.value,
     name: name.value,
-    owner: owner.value
-  }
-  const action = btnSave.innerHTML
+    owner: owner.value,
+  };
+  const action = btnSave.innerHTML;
   switch (action) {
-    case 'Editar':
-      pets[inputId.value] = data
+    case "Editar":
+      pets[inputId.value] = data;
       break;
     default:
-      pets.push(data)
+      pets.push(data);
       break;
   }
-  showPets()
-  resetData()
+  showPets();
+  resetData();
 }
 
-function edit(key){
+function edit(key) {
   return function handler() {
-    btnSave.innerText = "Editar"
-    mascota = pets[key]
-    name.value = mascota.name
-    owner.value = mascota.owner
-    type.value = mascota.type
-    inputId.value = key
-    console.log(mascota)
-  }
+    btnSave.innerText = "Editar";
+    mascota = pets[key];
+    name.value = mascota.name;
+    owner.value = mascota.owner;
+    type.value = mascota.type;
+    inputId.value = key;
+  };
 }
 
 function resetData() {
-  name.value = ''
-  owner.value = ''
-  type.value = ''
-  inputId.value = ''
-  btnSave.innerHTML = 'Crear'
+  name.value = "";
+  owner.value = "";
+  type.value = "";
+  inputId.value = "";
+  btnSave.innerHTML = "Crear";
 }
-showPets()
-form.onsubmit = sendData
-btnSave.onclick = sendData
+
+function deletePet(key) {
+  return function clickDelete() {
+    pets = pets.filter((mascota, keyMascota) => keyMascota !== key)
+    showPets();
+  };
+}
+showPets();
+form.onsubmit = sendData;
+btnSave.onclick = sendData;
