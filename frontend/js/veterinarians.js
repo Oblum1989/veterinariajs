3b9d8f17fcfd18146064f3e11098bc1bc84f6236
oscar blum <oscar.blum@gmail.com>
2021-06-19 17:33:27 -0500

    create crud for  veterinarians using api

--- frontend/js/veterinarians.js
+++ frontend/js/veterinarians.js
@@ -6,82 +6,95 @@ const country = document.getElementById("pais");
 const inputId = document.getElementById("inputId");
 const form = document.getElementById("form");
 const btnSave = document.getElementById("btnSave");
-let veterinarians = [
-  {
-    id: "63138513",
-    name: "Manchas",
-    lastName: "Perez",
-    country: "Colombia",
-  },
-  {
-    id: "15618613",
-    name: "firulais",
-    lastName: "Gomez",
-    country: "Perú",
-  },
-  {
-    id: "168515218",
-    name: "Furia",
-    lastName: "Soler",
-    country: "Perú",
-  },
-];
+const alertNode = document.getElementById('alert')
+const URL = "http://localhost:5000/veterinarians"
 
-function showVeterinarians() {
-  const htmlVeterinarians = veterinarians
-    .map(
-      (veterinarian, key) => `
-    <tr>
-      <th scope="row">${key}</th>
-      <td>${veterinarian.id}</td>
-      <td>${veterinarian.name}</td>
-      <td>${veterinarian.lastName}</td>
-      <td>${veterinarian.country}</td>
-      <td>
-        <button type="button" class="btn btn-success edit" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="far fa-edit"></i></button>
-        <button type="button" class="btn btn-danger delete"><i class="far fa-trash-alt"></i></button>
-      </td>
-    </tr>`
-    )
-    .join("");
-  listVeterinarians.innerHTML = htmlVeterinarians;
-  Array.from(document.getElementsByClassName("edit")).forEach(
-    (btnEdit, key) => (btnEdit.onclick = edit(key))
-  );
-  Array.from(document.getElementsByClassName("delete")).forEach(
-    (btnDelete, key) => (btnDelete.onclick = deleteVeterinarian(key))
-  );
+let veterinarians = [];
+
+async function showVeterinarians() {
+  try {
+    const response = await fetch(URL)
+    const veterinarianServer = await response.json()
+    if (Array.isArray(veterinarianServer)) {
+      veterinarians = veterinarianServer
+    }
+    if (veterinarians.length > 0) {
+      const htmlVeterinarians = veterinarians
+        .map(
+          (veterinarian, key) => `
+        <tr>
+          <th scope="row">${key}</th>
+          <td>${veterinarian.identification}</td>
+          <td>${veterinarian.name}</td>
+          <td>${veterinarian.lastname}</td>
+          <td>${veterinarian.country}</td>
+          <td>
+            <button type="button" class="btn btn-success edit" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="far fa-edit"></i></button>
+            <button type="button" class="btn btn-danger delete"><i class="far fa-trash-alt"></i></button>
+          </td>
+        </tr>`
+        )
+        .join("");
+      listVeterinarians.innerHTML = htmlVeterinarians;
+      Array.from(document.getElementsByClassName("edit")).forEach(
+        (btnEdit, key) => (btnEdit.onclick = edit(key))
+      );
+      Array.from(document.getElementsByClassName("delete")).forEach(
+        (btnDelete, key) => (btnDelete.onclick = deleteVeterinarian(key))
+      );
+    }else{
+      listVeterinarians.innerHTML = `<tr>
+        <td colspan="5">No tienes mascotas </td>
+      </tr>`
+    }
+  } catch (error) {
+    alertNode.classList.toggle("hide");
+    alertNode.classList.toggle("show");
+  }
 }
 
-function sendData(e) {
+async function sendData(e) {
   e.preventDefault();
-  const data = {
-    id: id.value,
-    name: name.value,
-    lastName: lastName.value,
-    country: country.value,
-  };
-  const action = btnSave.innerHTML;
-  switch (action) {
-    case "Editar":
-      veterinarians[inputId.value] = data;
-      break;
-    default:
-      veterinarians.push(data);
-      break;
+  try {
+    const data = {
+      identification: id.value,
+      name: name.value,
+      lastname: lastName.value,
+      country: country.value,
+    };
+    let method = "POST"
+    let urlSend = URL
+    const action = btnSave.innerHTML
+    if (action === "Editar") {
+      method = "PUT"
+      veterinarians[inputId.value] = data
+      urlSend = `${URL}/${inputId.value}`
+    }
+    const response = await fetch(urlSend, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    })
+    if (response.ok) {
+      showVeterinarians()
+      resetData()
+    }
+  } catch (error) {
+    alertNode.classList.toggle("hide");
+    alertNode.classList.toggle("show");
   }
-  showVeterinarians();
-  resetData();
 }
 
 function edit(key) {
   return function handler() {
     btnSave.innerText = "Editar";
-    mascota = veterinarians[key];
-    name.value = mascota.name;
-    lastName.value = mascota.lastName;
-    id.value = mascota.id;
-    country.value = mascota.country
+    veterinario = veterinarians[key];
+    name.value = veterinario.name;
+    lastName.value = veterinario.lastname;
+    id.value = veterinario.identification;
+    country.value = veterinario.country
     inputId.value = key;
   };
 }
@@ -96,11 +109,24 @@ function resetData() {
 }
 
 function deleteVeterinarian(key) {
-  return function clickDelete() {
-    veterinarians = veterinarians.filter((mascota, keyMascota) => keyMascota !== key);
-    showVeterinarians();
+  const urlSend = `${URL}/${key}`
+  return async function clickDelete() {
+    try {
+      const response = await fetch(urlSend, {
+        method: "DELETE",
+      })
+      if (response.ok) {
+        showVeterinarians()
+        resetData();
+      }
+    } catch (error) {
+      alertNode.classList.toggle("hide");
+      alertNode.classList.toggle("show");
+    }
   };
 }
+
 showVeterinarians();
+
 form.onsubmit = sendData;
 btnSave.onclick = sendData;
